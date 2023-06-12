4299b4aba7858187162b2db1a20166ea7be2f113
yokomon <marowmars@gmail.com>
2023-06-12 22:02:19 +0100

    Validations: Update form-validations

--- app/(site)/schemas/registration.ts
+++ app/(site)/schemas/registration.ts
@@ -1,12 +1,17 @@
 import * as Yup from "yup";
+import { Variant } from "../components/AuthForm";
 
-export const registrationSchema = Yup.object().shape({
-  name: Yup.string()
-    .required("Name is required")
-    .max(40, "Name must consists of 40 characters"),
-  email: Yup.string().email("Email is invalid").required("Email is required"),
-  password: Yup.string()
-    .min(8, "Password must have at least 8 characters")
-    .max(32, "Password must consist of 32 characters")
-    .required("Password is required"),
-});
+export const getRegistrationSchema = (variant: Variant) =>
+  Yup.object().shape({
+    name:
+      variant === "REGISTER"
+        ? Yup.string()
+            .required("Name is required")
+            .max(40, "Name must consists of 40 characters")
+        : Yup.string().max(40, "Name must consists of 40 characters"),
+    email: Yup.string().email("Email is invalid").required("Email is required"),
+    password: Yup.string()
+      .min(8, "Password must have at least 8 characters")
+      .max(32, "Password must consist of 32 characters")
+      .required("Password is required"),
+  });
