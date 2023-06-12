f97507ad0893d0aa31b2f67263ea5c31f7043310
yokomon <marowmars@gmail.com>
2023-06-12 22:00:59 +0100

    💄 Components: Social-button styles

--- app/(site)/components/SocialButton.tsx
+++ app/(site)/components/SocialButton.tsx
@@ -24,14 +24,14 @@ export const SocialButton: React.FC<SocialButtonProps> = ({
       type={type}
       onClick={onClick}
       className={clsx({
-        ["flex items-center justify-center duration-300 ring-1 ring-gray-300 ring-offset-1 bg-gray-100 p-3 text-gray-700 text-sm sm:text-base rounded-md hover:bg-gray-200 shadow-sm"]:
+        ["flex items-center justify-center duration-300 ring-1 ring-gray-300 ring-offset-1 bg-gray-100 p-2.5 text-gray-600 text-sm sm:text-base rounded-md hover:bg-gray-200 shadow-sm"]:
           true,
         ["w-full"]: fullWidth,
         ["opacity-50 cursor-not-allowed"]: disabled,
       })}
     >
-      <SocialIcon className="text-xl lg:text-2xl mr-6" />
-      {children}
+      <SocialIcon className="text-2xl lg:text-3xl" />
+      <div className="ml-6 sm:ml-9">{children}</div>
     </button>
   );
 };
