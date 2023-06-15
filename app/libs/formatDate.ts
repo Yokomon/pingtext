bbb501a4047c4ba79eca790eafe7f5039637c7b4
yokomon <marowmars@gmail.com>
2023-06-15 23:11:15 +0100

    Libs: Added new helper for date formatting

---
+++ app/libs/formatDate.ts
@@ -0,0 +1,19 @@
+import { format, isYesterday, isToday } from "date-fns";
+
+export const formatDate = (date: Date) => {
+  if (!date) return;
+  // Check if date is today
+  const isTodayDate = isToday(date);
+
+  // Check if input date was yesterday
+  const yesterday = isYesterday(date);
+
+  if (isTodayDate) {
+    const formattedDate = format(date, "p");
+    return formattedDate;
+  } else if (yesterday) {
+    return "Yesterday";
+  } else {
+    return format(date, "dd/MM/YYYY");
+  }
+};
