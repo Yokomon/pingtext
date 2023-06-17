9ed679893fb1efbffa48fee1944a7824d5411144
yokomon <marowmars@gmail.com>
2023-06-17 23:47:23 +0100

    🧑‍💻 Utils: Added helper with notification process.

---
+++ app/notifications/utils/processNotification.ts
@@ -0,0 +1,65 @@
+import { AxiosResponse } from "axios";
+import { SetStateAction } from "react";
+import { Friend, Notification, User } from "@prisma/client";
+import { UseMutationResult } from "@tanstack/react-query";
+
+import { DynamicStateType } from "@/types/DynamicState";
+
+type ProcessNotificationProps = {
+  friend: Friend | null;
+  user: User;
+  notification: Notification;
+  currentUser: User;
+  notificationMutation: UseMutationResult<
+    AxiosResponse<any, any>,
+    unknown,
+    {
+      notificationId: string;
+      isUserSender: boolean;
+    },
+    unknown
+  >;
+  openNotifications: boolean;
+  setOpenNotifications: (value: SetStateAction<boolean>) => void;
+  setDynamicState: (value: SetStateAction<DynamicStateType>) => void;
+  isUserSender: boolean;
+};
+
+const processNotification = ({
+  currentUser,
+  notificationMutation,
+  openNotifications,
+  setDynamicState,
+  setOpenNotifications,
+  isUserSender,
+  friend,
+  notification,
+  user,
+}: ProcessNotificationProps) => {
+  const isNotificationRead =
+    notification.friendRequestAccepted &&
+    ((currentUser.id === notification.senderId && notification.readBySender) ||
+      (currentUser.id === notification.recipientId &&
+        notification.readByRecipient));
+
+  if (isNotificationRead) return;
+
+  if (!notification.readByRecipient || isUserSender) {
+    notificationMutation.mutate({
+      notificationId: notification.id,
+      isUserSender,
+    });
+  }
+
+  setOpenNotifications(!openNotifications);
+  setDynamicState((state) => ({
+    ...state,
+    friend: friend,
+    message: notification.message,
+    user,
+    notificationId: notification.id,
+    userActions: isUserSender ? false : true,
+  }));
+};
+
+export default processNotification;
