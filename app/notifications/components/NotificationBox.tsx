64ea18c241585087c3fad92ee6ecb96c8c7cf7a7
yokomon <marowmars@gmail.com>
2023-06-17 23:45:49 +0100

    Components: NotificationBox update

--- app/notifications/components/NotificationBox.tsx
+++ app/notifications/components/NotificationBox.tsx
@@ -5,16 +5,19 @@ import clsx from "clsx";
 import { Avatar } from "@/app/components/Avatar";
 import { FullNotifications } from "@/types/NotificationTypes";
 import { EnableModalType } from "./NotificationsList";
-import { formatDate } from "@/app/libs/formatDate";
+import { formatDate } from "@/app/utils/formatDate";
+import { User } from "@prisma/client";
 
 interface NotificationBoxProps {
   data: FullNotifications[];
   enableModal: ({ user, notification }: EnableModalType) => Promise<void>;
+  currentUser: User;
 }
 
 export const NotificationBox: React.FC<NotificationBoxProps> = ({
   data,
   enableModal,
+  currentUser,
 }) => {
   if (!data.length)
     return (
@@ -23,41 +26,56 @@ export const NotificationBox: React.FC<NotificationBoxProps> = ({
       </div>
     );
 
-  const handleModal = ({ user, notification }: EnableModalType) => {
-    enableModal({ user, notification });
+  const handleModal = ({
+    user,
+    notification,
+    friend,
+    isUserSender,
+  }: EnableModalType) => {
+    enableModal({ user, notification, friend, isUserSender });
   };
 
   return (
     <>
-      {data.map(({ sender, ...notification }) => (
-        <div
-          key={notification.id}
-          onClick={() =>
-            handleModal({
-              user: sender,
-              notification,
-            })
-          }
-          className={clsx({
-            ["flex space-x-3 my-4 p-2"]: true,
-            ["hover:bg-gray-100 cursor-pointer rounded-md pl-2 duration-300 hover:shadow-sm"]:
-              !notification.message.match(/(accepted|rejected)/i),
-          })}
-        >
-          <Avatar currentUser={sender} />
-          <div className="relative w-full">
-            <div className="flex text-gray-600 items-center justify-between">
-              <h4 className="text-sm truncate lg:w-40">{sender.name}</h4>
-              <span className="text-xs text-gray-400">
-                {formatDate(notification.updatedAt)}
-              </span>
+      {data.map(({ sender, friend, recipient, ...notification }) => {
+        const isUserSender = sender.id === currentUser.id;
+        const enableBox =
+          currentUser.id !== notification.recipientId &&
+          !notification.readBySender;
+        return (
+          <div
+            key={notification.id}
+            onClick={() =>
+              handleModal({
+                user: sender,
+                notification,
+                friend,
+                isUserSender,
+              })
+            }
+            className={clsx({
+              ["flex space-x-3 my-4 p-2"]: true,
+              ["hover:bg-gray-100 cursor-pointer rounded-md pl-2 duration-300 hover:shadow-sm"]:
+                !notification.friendRequestAccepted || enableBox,
+            })}
+          >
+            <Avatar currentUser={isUserSender ? recipient! : sender} />
+            <div className="relative w-full">
+              <div className="flex text-gray-600 items-center justify-between">
+                <h4 className="text-sm truncate lg:w-40">
+                  {isUserSender ? recipient?.name : sender.name}
+                </h4>
+                <span className="text-xs text-gray-400">
+                  {formatDate(notification.updatedAt)}
+                </span>
+              </div>
+              <p className="mt-2 truncate text-xs w-44 text-gray-400">
+                {notification.message}
+              </p>
             </div>
-            <p className="mt-2 truncate text-xs w-44 text-gray-400">
-              {notification.message}
-            </p>
           </div>
-        </div>
-      ))}
+        );
+      })}
     </>
   );
 };
