13ff753daeebd35e499970bc1134356e4d8067a1
yokomon <marowmars@gmail.com>
2023-06-18 23:05:20 +0100

    ✅ Components: Update Avatar components and tests

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -10,7 +10,7 @@ interface AvatarProps {
 
 export const Avatar: React.FC<AvatarProps> = ({ currentUser }) => {
   const initials = useMemo(() => {
-    if ("name" in currentUser!) {
+    if (currentUser && "name" in currentUser) {
       return currentUser.name?.toUpperCase()?.split(" ") as string[];
     }
     return [];
@@ -19,7 +19,8 @@ export const Avatar: React.FC<AvatarProps> = ({ currentUser }) => {
   return (
     <div>
       <div className="w-12 relative flex justify-center items-center">
-        {"image" in currentUser && currentUser?.image ? (
+        {/* Check if current user and image exists */}
+        {currentUser && "image" in currentUser && currentUser.image !== null ? (
           <div className="relative rounded-full overflow-hidden ring-1 ring-offset-2 ring-sky-600 inline-block">
             <Image
               className="max-w-[10rem]"
@@ -31,6 +32,7 @@ export const Avatar: React.FC<AvatarProps> = ({ currentUser }) => {
             />
           </div>
         ) : (
+          // Render the initials of the user
           <div className="h-10 w-10 ring-sky-500 ring-1 ring-offset-2 rounded-full bg-gray-100 flex items-center justify-center">
             {initials.map((el, idx) => (
               <span className="text-gray-500 text-sm" key={idx}>
