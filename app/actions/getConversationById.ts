9bd3e2afdec381adda1fc4299428d5223bbbf0b2
yokomon <marowmars@gmail.com>
2023-06-21 23:16:49 +0100

    Actions: Get conversations

---
+++ app/actions/getConversationById.ts
@@ -0,0 +1,29 @@
+import prismadb from "../utils/prismadb";
+import { getCurrentUser } from "./getCurrentUser";
+
+export const getConversationById = async (pingId: string) => {
+  try {
+    const currentUser = await getCurrentUser();
+
+    if (!currentUser) return null;
+
+    const conversation = await prismadb.conversation.findFirst({
+      where: {
+        id: pingId,
+      },
+      include: {
+        users: {
+          where: {
+            id: {
+              notIn: currentUser.id,
+            },
+          },
+        },
+      },
+    });
+
+    return conversation;
+  } catch (error) {
+    return null;
+  }
+};
---
+++ app/actions/getConversations.ts
@@ -0,0 +1,34 @@
+import prismadb from "../utils/prismadb";
+import { getCurrentUser } from "./getCurrentUser";
+
+export const getConversations = async () => {
+  try {
+    const currentUser = await getCurrentUser();
+
+    if (!currentUser?.id) return [];
+
+    const userConversations = await prismadb.conversation.findMany({
+      orderBy: {
+        lastPingAt: "desc",
+      },
+      where: {
+        userIds: {
+          has: currentUser.id,
+        },
+      },
+      include: {
+        users: {
+          where: {
+            email: {
+              notIn: currentUser.email,
+            },
+          },
+        },
+      },
+    });
+
+    return userConversations;
+  } catch (error) {
+    return [];
+  }
+};
