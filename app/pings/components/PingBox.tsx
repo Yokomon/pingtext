8ab977c24831a056541bc296cd020e332cdcca17
yokomon <marowmars@gmail.com>
2023-06-25 22:16:03 +0100

    Components: Removed default text in PingBox

--- app/pings/components/PingBox.tsx
+++ app/pings/components/PingBox.tsx
@@ -1,31 +1,72 @@
 "use client";
 
+import clsx from "clsx";
+
 import { Avatar } from "@/app/components/Avatar";
-import { User } from "@prisma/client";
+import { decryptMessage } from "@/app/utils/encryption";
+import { formatDate } from "@/app/utils/formatDate";
+import { Pings, User } from "@prisma/client";
+import { useRouter } from "next/navigation";
 
 interface PingBoxProps {
-  user: User;
+  otherUser: User;
+  pings: Pings[];
+  lastPingAt: Date;
+  currentUser: User;
+  conversationId: string;
 }
 
-export const PingBox: React.FC<PingBoxProps> = ({ user }) => {
+export const PingBox: React.FC<PingBoxProps> = ({
+  otherUser,
+  pings,
+  lastPingAt,
+  currentUser,
+  conversationId,
+}) => {
+  const router = useRouter();
+
+  const lastMessage = pings[pings.length - 1];
+
+  const unreadMessages = pings.reduce((prev, value) => {
+    if (value.receiverIds.indexOf(currentUser.id) === -1) {
+      prev = prev + 1;
+    }
+    return prev;
+  }, 0);
+
+  const handleRedirect = () => {
+    return router.push(`/pings/${conversationId}`);
+  };
+
   return (
-    <div className="flex my-3 w-full cursor-pointer hover:bg-gray-100 hover:text-black text-gray-700 dark:text-white dark:hover:text-gray-800 p-3 duration-300 rounded-md space-x-2 items-center">
-      <Avatar currentUser={user} />
+    <div
+      onClick={handleRedirect}
+      className={
+        "flex my-3 w-full cursor-pointer hover:bg-gray-100 hover:text-black text-gray-700 dark:text-white dark:hover:text-gray-800 p-3 duration-300 rounded-md space-x-2 items-center"
+      }
+    >
+      <Avatar currentUser={otherUser} />
       <div className="min-w-0 w-full">
         <div className="flex items-start justify-between w-full mb-2">
-          <h3 className="text-sm">{user.name}</h3>
-          <p className="text-xs mt-0.5">11:35pm</p>
+          <h3 className="text-sm">{otherUser.name}</h3>
+          <p className="text-xs mt-0.5">
+            {lastPingAt ? formatDate(lastPingAt) : null}
+          </p>
         </div>
         <div className="flex justify-between items-center">
-          <p className="truncate w-40 text-sm text-gray-500">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse labore
-            dolores, ipsam ullam quos consequuntur sapiente, beatae modi quod
-            assumenda veniam odio? Unde excepturi culpa vitae temporibus
-            architecto quidem inventore.
+          <p
+            className={clsx({
+              ["truncate w-40 text-sm text-gray-400"]: true,
+              ["text-gray-700 dark:text-inherit"]: unreadMessages,
+            })}
+          >
+            {decryptMessage(lastMessage.body)}
           </p>
-          <span className="text-xs text-white bg-red-600 w-fit p-2 h-5 justify-center flex items-center rounded-full">
-            2
-          </span>
+          {unreadMessages ? (
+            <span className="text-xs text-white bg-red-600 w-fit p-2 h-5 justify-center flex items-center rounded-full">
+              {unreadMessages}
+            </span>
+          ) : null}
         </div>
       </div>
     </div>
