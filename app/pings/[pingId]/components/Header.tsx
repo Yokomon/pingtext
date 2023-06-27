6ee5cfca68edfd237aba96e59cf39258eb8b6ce3
yokomon <marowmars@gmail.com>
2023-06-27 23:22:27 +0100

    Components: Improved Header activity status

--- app/pings/[pingId]/components/Header.tsx
+++ app/pings/[pingId]/components/Header.tsx
@@ -5,12 +5,18 @@ import { MdArrowBack } from "@react-icons/all-files/md/MdArrowBack";
 
 import { Avatar } from "@/app/components/Avatar";
 import { User } from "@prisma/client";
+import { useStore } from "@/app/hooks/useStore";
+import useChannelList from "@/app/hooks/useChannelList";
 
 interface HeaderProps {
   otherUser: User;
 }
 
 export const Header: React.FC<HeaderProps> = ({ otherUser }) => {
+  const members = useStore(useChannelList, (state) => state.members);
+
+  const isOnline = members.has(otherUser.email);
+
   return (
     <div className="flex items-center p-4 h-20 justify-between border-b border-gray-100 dark:bg-neutral-900 dark:border-gray-50/10 w-full shadow-md">
       <div className="p-2 hover:bg-sky-100 rounded-full duration-500 lg:hidden">
@@ -23,7 +29,9 @@ export const Header: React.FC<HeaderProps> = ({ otherUser }) => {
         <Avatar currentUser={otherUser} />
         <div className="relative">
           <h4 className="text-gray-700 dark:text-gray-200">{otherUser.name}</h4>
-          <p className="text-sm text-gray-500 ">Available</p>
+          <p className="text-xs text-gray-500 dark:text-gray-400 mt-1">
+            {isOnline ? "Available" : "Offline"}
+          </p>
         </div>
       </div>
       <div className="p-2 rounded-full hover:bg-sky-100 duration-500 text-gray-500 hover:text-sky-600">
