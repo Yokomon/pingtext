ea8addc3a3633c63a6d0d7a8a60e5758cacb246d
yokomon <marowmars@gmail.com>
2023-07-04 22:44:18 +0100

    Libs: Removed circular imports

--- app/lib/pusher.ts
+++ app/lib/pusher.ts
@@ -1,6 +1,8 @@
-import PusherServer from "pusher";
+import Pusher from "pusher";
 
-export const pusherServer = new PusherServer({
+const PusherServer = require("pusher");
+
+export const pusherServer: Pusher = new PusherServer({
   appId: process.env.NEXT_PUBLIC_PUSHER_APP_ID!,
   key: process.env.NEXT_PUBLIC_PUSHER_KEY!,
   secret: process.env.NEXT_PUBLIC_PUSHER_SECRET!,
