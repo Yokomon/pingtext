5c0f68df497c11666f65425ff0d72203dd23ef3d
yokomon <marowmars@gmail.com>
2023-07-18 18:42:30 +0100

    Components: Refactored Avatar component

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -8,13 +8,21 @@ import { useStore } from "../hooks/useStore";
 import useChannelList from "../hooks/useChannelList";
 
 interface AvatarProps {
-  currentUser: User;
+  currentUser: User | null;
+  removeBadge?: boolean;
+  imageWidth?: number;
+  imageHeight?: number;
 }
 
-export const Avatar: React.FC<AvatarProps> = ({ currentUser }) => {
+export const Avatar: React.FC<AvatarProps> = ({
+  currentUser,
+  removeBadge,
+  imageWidth,
+  imageHeight,
+}) => {
   const members = useStore(useChannelList, (state) => state.members);
 
-  const isActive = members.has(currentUser.email);
+  const isActive = members.has(currentUser?.email!);
 
   const initials = useMemo(() => {
     if (currentUser && "name" in currentUser) {
@@ -25,21 +33,22 @@ export const Avatar: React.FC<AvatarProps> = ({ currentUser }) => {
 
   return (
     <div>
-      <div className="w-12 relative flex justify-center items-center">
+      <div className="relative flex justify-center items-center">
         {/* Check if current user and image exists */}
         {currentUser && "image" in currentUser && currentUser.image !== null ? (
           <div className="relative rounded-full overflow-hidden ring-1 dark:ring-0 dark:ring-offset-0 ring-offset-2 ring-sky-600 inline-block">
             <Image
-              className="max-w-[10rem]"
-              width={40}
+              unoptimized={true}
+              width={imageWidth ?? 40}
+              height={imageHeight ?? 40}
               src={currentUser.image}
               alt={"avatar"}
-              height={40}
               priority
+              quality={100}
             />
           </div>
         ) : (
-          // Render the initials of the user
+          // Render the initials of the user4
           <div className="h-10 w-10 ring-sky-500 ring-1 dark:ring-1 dark:ring-gray-500 ring-offset-2 rounded-full bg-gray-100 dark:bg-white flex items-center justify-center">
             {initials.map((el, idx) => (
               <span className="text-gray-500 text-sm" key={idx}>
@@ -48,11 +57,13 @@ export const Avatar: React.FC<AvatarProps> = ({ currentUser }) => {
             ))}
           </div>
         )}
-        {isActive ? (
-          <span className="absolute block bg-teal-400 right-1 top-7 rounded-full p-1 ring-1 dark:ring-0 dark:ring-offset-0 ring-white dark:ring-gray-400 ring-offset-2" />
-        ) : (
-          <span className="absolute block bg-slate-500 right-1 top-7 rounded-full p-1 ring-1 dark:ring-0 dark:ring-offset-0 ring-white dark:ring-gray-400 ring-offset-2" />
-        )}
+        {!removeBadge ? (
+          isActive ? (
+            <span className="absolute block bg-teal-400 right-1 top-7 rounded-full p-1 ring-1 dark:ring-0 dark:ring-offset-0 ring-white dark:ring-gray-400 ring-offset-2" />
+          ) : (
+            <span className="absolute block bg-slate-500 right-1 top-7 rounded-full p-1 ring-1 dark:ring-0 dark:ring-offset-0 ring-white dark:ring-gray-400 ring-offset-2" />
+          )
+        ) : null}
       </div>
     </div>
   );
