09286e7fe573104b5bfe9e4bde88f8224bc8f0d2
yokomon <marowmars@gmail.com>
2023-07-20 22:26:46 +0100

    Components: Videocall modified.

--- app/calls/[callId]/components/VideoCall.tsx
+++ app/calls/[callId]/components/VideoCall.tsx
@@ -10,7 +10,6 @@ import { useRTCHooks } from "./hooks/useRTCHooks";
 import { Avatar } from "@/app/components/Avatar";
 import { User } from "@prisma/client";
 import { MixedCallerTypes } from "@/types/CallTypes";
-import { RefObject } from "react";
 
 interface VideoCallProps {
   callDetails: MixedCallerTypes;
@@ -25,24 +24,25 @@ export const VideoCall: React.FC<VideoCallProps> = ({
 }) => {
   const {
     userVideo,
-    otherUserVideo,
+    partnerVideo,
     toggleCamera,
     toggleMic,
     enableCamera,
     enableMic,
+    cameraStatus,
   } = useRTCHooks(callDetails.id);
 
   return (
-    <div className="p-3 relative">
-      <div className="relative">
+    <div className="p-3 relative h-full">
+      <div className="relative h-full">
         <div
           className={clsx({
             ["absolute flex justify-center items-center w-full h-full"]: true,
-            ["hidden"]: enableCamera,
+            ["hidden"]: cameraStatus,
           })}
         >
           <Avatar
-            currentUser={currentUser}
+            currentUser={otherUser}
             removeBadge
             imageHeight={130}
             imageWidth={130}
@@ -50,64 +50,68 @@ export const VideoCall: React.FC<VideoCallProps> = ({
         </div>
         <video
           autoPlay
-          ref={userVideo}
-          className="w-full h-full border rounded-md bg-rose-500"
-          muted
+          ref={partnerVideo}
+          className="w-full h-full border rounded-md"
         />
-        <div
-          onClick={toggleCamera}
-          className={clsx({
-            ["p-4 w-fit duration-500 absolute bottom-0 right-1/2 rounded-full my-5 cursor-pointer"]:
-              true,
-            [" bg-rose-50 hover:bg-rose-100"]: enableCamera,
-            ["bg-gray-50 hover:bg-gray-100"]: !enableCamera,
-          })}
-        >
-          {enableCamera ? (
-            <BsCameraVideoFill size={32} className="fill-rose-500" />
-          ) : (
-            <BsCameraVideoOffFill size={32} className="fill-gray-400" />
-          )}
-        </div>
-        <div
-          onClick={toggleMic}
-          className={clsx({
-            ["p-3 w-fit duration-500 absolute bottom-0 right-[42%] rounded-full my-5 cursor-pointer"]:
-              true,
-            [" bg-rose-50 hover:bg-rose-100"]: enableMic,
-            ["bg-gray-50 hover:bg-gray-100"]: !enableMic,
-          })}
-        >
-          {enableMic ? (
-            <PiMicrophoneFill size={28} className="fill-rose-500" />
-          ) : (
-            <PiMicrophoneSlashFill size={28} className="fill-gray-400" />
-          )}
-        </div>
       </div>
 
-      <div className="w-full flex justify-end -mb-10">
-        <div className="w-60 h-48 absolute bottom-0 right-6">
+      <div className="w-full flex justify-end">
+        <div className="w-[27rem] h-60 absolute bottom-4 right-6">
           <div
             className={clsx({
-              ["absolute flex justify-center items-center w-full h-full"]: true,
-              ["hidden"]: true,
+              ["absolute flex justify-center items-center pb-7 w-full h-full"]:
+                true,
+              ["hidden"]: enableCamera,
             })}
           >
             <Avatar
-              currentUser={otherUser}
+              currentUser={currentUser}
               removeBadge
-              imageHeight={50}
-              imageWidth={50}
+              imageHeight={67}
+              imageWidth={67}
             />
           </div>
           <video
+            ref={userVideo}
             autoPlay
-            ref={otherUserVideo as RefObject<HTMLVideoElement>}
+            muted
             className={clsx({
-              ["border-2 border-gray-300 rounded-md h-full bg-black"]: true,
+              ["border-2 border-gray-300 rounded-md object-cover h-full w-full bg-black"]:
+                true,
             })}
           />
+          <div className="absolute bottom-0 flex items-center justify-center w-full space-x-3">
+            <div
+              onClick={toggleCamera}
+              className={clsx({
+                ["p-4 w-fit duration-500 rounded-full my-5 cursor-pointer"]:
+                  true,
+                [" bg-rose-50 hover:bg-rose-100"]: enableCamera,
+                ["bg-gray-50 hover:bg-gray-100"]: !enableCamera,
+              })}
+            >
+              {enableCamera ? (
+                <BsCameraVideoFill size={14} className="fill-rose-500" />
+              ) : (
+                <BsCameraVideoOffFill size={14} className="fill-gray-400" />
+              )}
+            </div>
+            <div
+              onClick={toggleMic}
+              className={clsx({
+                ["p-3 w-fit duration-500 rounded-full my-5 cursor-pointer"]:
+                  true,
+                [" bg-rose-50 hover:bg-rose-100"]: enableMic,
+                ["bg-gray-50 hover:bg-gray-100"]: !enableMic,
+              })}
+            >
+              {enableMic ? (
+                <PiMicrophoneFill size={14} className="fill-rose-500" />
+              ) : (
+                <PiMicrophoneSlashFill size={14} className="fill-gray-400" />
+              )}
+            </div>
+          </div>
         </div>
       </div>
     </div>
