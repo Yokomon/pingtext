534ee5e0712481b0ed71f3be21bc58bc4b8fb8c3
yokomon <marowmars@gmail.com>
2023-07-24 23:49:20 +0100

    Layout: Improved Call Layout page

--- app/calls/layout.tsx
+++ app/calls/layout.tsx
@@ -1,5 +1,4 @@
 import { getAllFriends } from "../actions/getAllFriends";
-import { getCurrentUser } from "../actions/getCurrentUser";
 import SideBar from "../components/sidebar/Sidebar";
 import { CallList } from "./components/CallList";
 
@@ -9,11 +8,9 @@ interface LayoutProps {
 
 const Layout = async ({ children }: LayoutProps) => {
   const friends = await getAllFriends();
-  const currentUser = await getCurrentUser();
   return (
-    //@ts-expect-error Server component
     <SideBar>
-      <CallList friends={friends} currentUser={currentUser} />
+      <CallList friends={friends} />
       {children}
     </SideBar>
   );
