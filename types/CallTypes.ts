5434517394091713a035ac648a6528416f7ae17d
yokomon <marowmars@gmail.com>
2023-07-24 23:53:40 +0100

    Types: New types added for Calls

--- types/CallTypes.ts
+++ types/CallTypes.ts
@@ -2,5 +2,11 @@ import { Call, User } from "@prisma/client";
 
 export type MixedCallerTypes = Call & {
   caller: User;
-  callReceiver?: User
+  callReceiver?: User;
+};
+
+export type NewCall = {
+  userId: string;
+  name: string;
+  callId: string;
 };
